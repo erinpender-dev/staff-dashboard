80d5a60aea4782832ceb2eeca01588f4e4d1cbd1
erinpender-dev <erinpender.rise@gmail.com>
2026-03-24 18:47:54 -0500

    Update order-details.js

--- api/order-details.js
+++ api/order-details.js
@@ -146,6 +146,11 @@ async function readPrivateJson(orderId) {
 
 export default async function handler(req, res) {
   setCors(req, res);
+  res.setHeader("Cache-Control", "no-store, no-cache, must-revalidate, proxy-revalidate");
+res.setHeader("Pragma", "no-cache");
+res.setHeader("Expires", "0");
+res.setHeader("Surrogate-Control", "no-store");
+
 
   if (req.method === "OPTIONS") {
     return res.status(200).end();
